eccca447995769f76a2b0f6cbc7288a552d0061f
James White <cyberyt@gmail.com>
2015-08-12 17:04:15 +0100

    Html built, calc prompts added.

---
+++ calcit.js
@@ -0,0 +1,8 @@
+var op = prompt('choose an operation');
+console.log('op', op);
+var num - prompt('choose a number');
+console.log('num', num);
+
+var goodNum = parseFloat(num);
+
+alert("Why is your console not open fam?");
