05bc5d13255933dbe2f24e06c23a20f3bdd2f397
James White <cyberyt@gmail.com>
2015-08-12 18:18:44 +0100

    add works

--- calcit.js
+++ calcit.js
@@ -1,8 +1,23 @@
-var op = prompt('choose an operation');
+var op = prompt('Choose an operation\nYou can perform (A)ddition, (S)ubtraction, (M)ultiplication, or (D)ivision');
   console.log('op', op);
-var num = prompt('choose a number');
-  console.log('num', num);
+var num1 = prompt('Choose your first number');
+  console.log('num1', num1);
+var num2 = prompt('Your second number is?')
+  console.log('num2', num2);
 
-var goodNum = parseFloat(num);
+var goodNum1 = parseFloat(num1);
+  console.log('gdnm1', goodNum1);
+var goodNum2 = parseFloat(num2);
+  console.log('gdnm2', goodNum2);
 
-alert("Why is your console not open fam?");
+
+// if (isNaN(goodNum1) === true) {
+//   alert('You have to pick a number for this to work. Try again');
+// }
+// else {
+// }
+while (op === 'A') {
+  var answer = (goodNum1 + goodNum2)
+  alert('The answer is ' + answer)
+  break;
+}
