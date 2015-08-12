8f326241400f54f365c0107a835562cda7941c39
James White <cyberyt@gmail.com>
2015-08-12 21:40:36 +0100

    added loop

--- calcit.js
+++ calcit.js
@@ -1,6 +1,8 @@
+var looping = true;
+while (looping == true) {
 var op = prompt('Choose an operation. This calculator can do\n(A)ddition +, (S)ubtraction -, (M)ultiplication *, (D)ivision /, (P)ower ^, or s(Q)uareroute \u221A').toUpperCase(); 
   console.log('op', op);
-  var num1 = prompt('Choose your first number');
+var num1 = prompt('Choose your first number');
   console.log('num1', num1);
 if (op != 'Q') {
   var num2 = prompt('Your second number is?')
@@ -30,35 +32,5 @@ else if (op === 'P' || op === '^') {
 else if (op === 'Q') {
   var answer = Math.sqrt(goodNum1);
 }
-
-// if (isNaN(goodNum1) === true) {
-//   alert('You have to pick a number for this to work. Try again');
-// }
-// else {
-// }
-
-// //Below not great symantically?
-// while (op === 'A') {
-//   var answer = (goodNum1 + goodNum2)
-//   break;
-// }
-// while (op === 'S') {
-//   var answer = (goodNum1 - goodNum2)
-//   break;
-// }
-// while (op === 'M') {
-//   var answer = (goodNum1 * goodNum2)
-//   break;
-// }
-// while (op === 'D') {
-//   var answer = (goodNum1 / goodNum2)
-//   break;
-// }
 alert('The answer is ' + answer);
-// var again = prompt('Would you like to perform another calculation? Y/N')
-// if (again = 'Y') {
-//   Location.reload(true);
-// }
-// else {
-
-// }
+}
