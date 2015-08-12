d14ef2bfce7a2d5297b47b67782d967b71a89ed5
James White <cyberyt@gmail.com>
2015-08-12 20:22:35 +0100

    added if statement taking away num2 prompt if SqRt

--- calcit.js
+++ calcit.js
@@ -1,10 +1,11 @@
 var op = prompt('Choose an operation. This calculator can do\n(A)ddition +, (S)ubtraction -, (M)ultiplication *, (D)ivision /, (P)ower ^, or s(Q)uareroute \u221A').toUpperCase(); 
   console.log('op', op);
-var num1 = prompt('Choose your first number');
+  var num1 = prompt('Choose your first number');
   console.log('num1', num1);
-var num2 = prompt('Your second number is?')
+if (op != 'Q') {
+  var num2 = prompt('Your second number is?')
   console.log('num2', num2);
-
+}
 var goodNum1 = parseFloat(num1);
   console.log('gdnm1', goodNum1);
 var goodNum2 = parseFloat(num2);
